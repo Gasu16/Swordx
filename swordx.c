fed1b4be2a47a0f62a65ee3903851235ac782e24
Matteo Grammatico <kizaru774@gmail.com>
2018-07-30 14:50:58 +0200

    Add files via upload

--- swordx.c
+++ swordx.c
@@ -9,7 +9,7 @@
  * Author: matteo
  *
  * Created on 25 giugno 2018, 12.01
- * Ultima modifica 28 luglio 2018, 20.52
+ * Ultima modifica 30 luglio 2018, 14.49
  */
 
 #include <stdio.h>
@@ -23,19 +23,20 @@
 #include <getopt.h>
 #include <dirent.h>
 #include "occorrenze.h"
-
+//#include <glib-2.0/glib.h>
 
 #define LEN 256
 #define DIM 256
 
 int opt; // Opzione scelta
 
+
 const char *short_options = "hrfeamisl";
 const char *path = "/home/matteo/NetBeansProjects/ProgettoSistemiOperativi/./*";
 
 struct option long_options[] = {
     {"help", no_argument, NULL, 'h'}, // V
-    {"recursive", required_argument, NULL, 'r'}, // X
+    {"recursive", required_argument, NULL, 'r'}, // V
     {"follow", required_argument, NULL, 'f'}, // X
     {"explude", required_argument, NULL, 'e'}, // X
     {"alpha", required_argument, NULL, 'a'}, // X
@@ -143,10 +144,10 @@ void listdir(const char *name, int indent){
     // indent: numero di indentazioni
     DIR *dir; // Directory dove ci troviamo noi
     struct dirent *entry; // Contenuto della directory
-
+    
     if (!(dir = opendir(name))) // Se non riesce ad aprire la directory usciamo
         return;
-
+    
     while ((entry = readdir(dir)) != NULL) { // Mentre scorre il contenuto della directory...
         if (entry->d_type == DT_DIR) { // Se il contenuto corrisponde a una directory...
             char bufpath[1024]; // buffer
@@ -159,11 +160,37 @@ void listdir(const char *name, int indent){
             printf("%*s[%s]\n", indent, "", entry->d_name); 
             listdir(bufpath, indent + 2); // Ricorsiva...
         } else { // Altrimenti se e' un file...
-        //   printf("%*s- %s\n", indent, "", entry->d_name);
+            //   printf("%*s- %s\n", indent, "", entry->d_name);
         }
     }
     closedir(dir);
 }
+
+char *recover_filename(FILE *f) {
+    int fd;
+    char fd_path[255];
+    char *filename = malloc(255);
+    ssize_t n;
+    
+    fd = fileno(f);
+    sprintf(fd_path, "/proc/self/fd/%d", fd);
+    n = readlink(fd_path, filename, 255);
+    if (n < 0){
+        return NULL;
+    }
+    
+    filename[n] = '\0'; // Carattere di terminazione
+    printf("Nome file: \n%s\n", filename);
+    return filename;
+}
+
+
+
+void generate_log_file(FILE *logFile, FILE *fileIN){
+    char nomeFile = recover_filename(fileIN);
+    fprintf(logFile, nomeFile);
+}
+
 
 int main(int argc, char** argv) {    
     
@@ -176,17 +203,33 @@ int main(int argc, char** argv) {
     FILE *file = fopen(argv[2], "r"); // File di input
     FILE *fileOUTPUT = fopen("swordx.out", "w+"); // File di output
     
+    // Array dove metteremo tutti i file (solo quelli di input) analizzati (ci serve per il log)
+    FILE *inputFiles[argc - 1]; 
     
     while((opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1){
         switch(opt){
-            case 'h':
+            case 'h': // ./swordx -h input.txt swordx.out
                 getHelp();    
-                splitFile(file, fileOUTPUT); // ./swordx -h input.txt swordx.out
-                break;
-            case 'r':
-                printf("\nRecursive\n");
-                listdir(".", 0);
+                splitFile(file, fileOUTPUT); 
+                int i;
+                for(i = 2; i < argc-1; i++){
+                    if(strcmp(argv[i], "swordx.out")){
+                        
+                        //argv[i] = NULL; // Eliminiamo swordx.out dall'array
+                        /* Non ci sarebbe bisogno di questo controllo poiche' 
+                         * sappiamo che swordx.out sara' l'ultimo parametro
+                         * quindi basta impostare il for fino a argc - 1
+                         * escludendo cosi' automaticamente swordx.out */
+                    }
+                    printf("ARGV[i]: %s\n", argv[i]);
+                    inputFiles[i] = fopen(argv[i], "r");
+                    printf("Puntatore[i]: %p\n", inputFiles[i]);
+                }
                 
+                break;
+            case 'r': // ./swordx -r /path/to/open
+                printf("\nRecursive\n");
+                listdir(argv[2], 0);
                 break;
             case 'f':
                 printf("\nFollow\n");
@@ -208,13 +251,25 @@ int main(int argc, char** argv) {
                 break;
             case 'l':
                 // Per funzionare il nome del file di log dev'essere il terzo parametro
-                // ./swordx -l dati.log
+                // ./swordx -l input.txt dati.log
                 printf("\nGenerazione file di log in corso...\n");
-                FILE *logFile = fopen(argv[2], "a+"); // Usiamo il metodo a+ cioe' appending
+                FILE *logFile = fopen(argv[3], "a+"); // Usiamo il metodo a+ cioe' appending
                 if(logFile == NULL){
                     printf("\nFile di log non presente\n");
                 }
-                fprintf(logFile, "Sto loggando...");
+                
+                int j;
+                for(j = 2; j < argc-1; j++){
+                    // Lista dei file in input analizzati
+                    fprintf(logFile, "\n%s\n", argv[j]);        
+                    printf("ARGV[j]: %s\n", argv[j]);
+                    inputFiles[j] = fopen(argv[j], "r");
+                    printf("Puntatore[j]: %p\n", inputFiles[j]);
+                    
+                }
+                char nome_file = recover_filename(file);
+                
+                
                 break;
         }
     }
